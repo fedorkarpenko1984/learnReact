67616fb5f2bab8fec77ec377b35cf413a3d83760
fedorkarpenko1984 <73883259+fedorkarpenko1984@users.noreply.github.com>
2021-03-08 00:39:17 +0300

    Add some CSS styles to components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import TodoList from './ToDo/TodoList'
+
+
 
 function App() {
+  const todos = [
+      {id: 1, complited: false, title: 'Купить хлеб'},
+      {id: 2, complited: false, title: 'Купить масло'},
+      {id: 3, complited: false, title: 'Купить молоко'}
+  ]
+
   return (
     <div className='wrapper'>
-
+      <h1>React JS</h1>
+      <TodoList todos = {todos} />
     </div>
   );
 }
