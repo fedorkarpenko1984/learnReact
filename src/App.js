0105112a7e4fc9ed5f48c6acdbb9492f49ca7c4a
fedorkarpenko1984 <73883259+fedorkarpenko1984@users.noreply.github.com>
2021-03-07 20:54:41 +0300

    TodoList component added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import React from 'react'
+import TodoList from './ToDo/TodoList'
 
 function App() {
   return (
     <div className='wrapper'>
-
+      <h1>React JS</h1>
+      <TodoList/>
     </div>
   );
 }
