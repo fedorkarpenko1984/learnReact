344960c0ed53ce2b53abb35eb86b198ae60fa4b3
fedorkarpenko1984 <73883259+fedorkarpenko1984@users.noreply.github.com>
2021-03-08 07:56:39 +0300

    Added CSS styles for TodoList and TodoItem components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import TodoList from './ToDo/TodoList'
 
 
 
 function App() {
-  const todos = [
+  let todos = [
       {id: 1, complited: false, title: 'Купить хлеб'},
       {id: 2, complited: false, title: 'Купить масло'},
       {id: 3, complited: false, title: 'Купить молоко'}
   ]
 
+  function toggleTodo(id) {
+      todos = todos.map(todo => {
+          if (todo.id === id) {
+              todo.complited = !todo.complited
+          }
+          return todo
+      })
+  }
+
   return (
     <div className='wrapper'>
       <h1>React JS</h1>
-      <TodoList todos = {todos} />
+      <TodoList todos = {todos} onToggle={toggleTodo}/>
     </div>
   );
 }
